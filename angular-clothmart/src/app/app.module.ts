c8709546448a4b15f845739176a870ea56e3d003
Shruti <Shruti@LAPTOP-RRDKDUJF>
2020-06-09 12:18:39 +0700

    completed search by category feature using routing

--- angular-clothmart/src/app/app.module.ts
+++ angular-clothmart/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ClothListComponent } from './components/cloth-list/cloth-list.component';
 import { ClothesService } from './services/clothes.service';
+import { RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [ 
+  {path: 'clothes' , component : ClothListComponent},
+  {path: 'category/:id' , component : ClothListComponent},
+  {path: '' ,redirectTo : '/clothes' , pathMatch : 'full'},
+  {path: '**' , component : PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClothListComponent
+    ClothListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [
     ClothesService
