c2f335d50e0b8792b6bf9346ea1fa31e7d151967
Shruti <Shruti@LAPTOP-RRDKDUJF>
2020-06-10 10:59:13 +0700

    created clothes category component and displayed dynamic categories

--- Shopping-Application/src/main/java/com/example/Shopping/Application/repository/CategoryRepository.java
+++ Shopping-Application/src/main/java/com/example/Shopping/Application/repository/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.example.Shopping.Application.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.Shopping.Application.entity.Category;
 
